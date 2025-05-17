9a80c1cff14ce2b5bfa8c0c2a3edd04ac64a9ac9
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-05-18 02:41:47 +0530

    refactor-2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : import.meta.env.VITE_BASE_PATH || '/jagan.github.io',
+  base: "/", // For Vercel, this should always be just "/"
 });
